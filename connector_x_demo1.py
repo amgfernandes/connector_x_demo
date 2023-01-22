da1c22ea7bc51fca52d0daa2afa293abf06e77a5
amgfernandes <amgfernandes@gmail.com>
2023-01-22 18:09:03 +0100

    query lazy df

--- connector_x_demo1.py
+++ connector_x_demo1.py
@@ -2,8 +2,11 @@
 import polars as pl
 #%%
 
-conn = ""postgresql://username:password@server:port/database"
-query = "SELECT * FROM dvdrental.public.actor;"
-
-pl.read_sql(query, conn)
+conn = "postgresql://postgres:sql@localhost:5432/dvdrental"
+query = "SELECT * FROM dvdrental.public.actor"
+#%%
+df= (pl.read_sql(query, conn).lazy())
+print(df.head(10))
+# %%
+df.head().collect()
 # %%
